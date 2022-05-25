b910c587bd75a89d5a868361e901443b14a5d16a
Ben Stone <benstone@uiowa.edu>
2022-05-25 09:59:11 -0500

    Move queries into their own directory
    Did so to clean up file structure and main method a little bit

--- LearningLinq/Program.cs
+++ LearningLinq/Program.cs
@@ -1,4 +1,4 @@
-﻿using LearningLinq.Utils;
+﻿using LearningLinq.Queries;
 
 namespace LearningLinq;
 
@@ -6,19 +6,7 @@ internal class Program
 {
     private static void Main()
     {
-        BasicIntQueries();
-    }
-
-    private static void BasicIntQueries()
-    {
-        var scores = QueryUtils.CreateIntRange();
-
-        var greaterThanEightyQuery = scores.Where(x => x > 80);
-        QueryUtils.DisplayQuery(greaterThanEightyQuery, "Greater than 80");
-
-        var toSeven = QueryUtils.CreateIntRange(stop: 7);
-        var queryEvens = toSeven.Where(x => x % 2 == 0);
-        QueryUtils.DisplayQuery(queryEvens, "Even numbers");
-
+        Console.WriteLine("Student and Teacher queries:");
+        StudentTeacherQueries.Query();
     }
 }
---
+++ LearningLinq/Queries/BasicIntQueries.cs
@@ -0,0 +1,19 @@
+﻿using LearningLinq.Utils;
+
+namespace LearningLinq.Queries;
+
+public class BasicIntQueries
+{
+    public static void Query()
+    {
+        var scores = QueryUtils.CreateIntRange();
+
+        var greaterThanEightyQuery = scores.Where(x => x > 80);
+        QueryUtils.DisplayQuery(greaterThanEightyQuery, "Greater than 80");
+
+        var toSeven = QueryUtils.CreateIntRange(stop: 7);
+        var queryEvens = toSeven.Where(x => x % 2 == 0);
+        QueryUtils.DisplayQuery(queryEvens, "Even numbers");
+
+    }
+}
