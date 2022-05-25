e8c35eb6904afae5ceded3dec5dc8da07fe5fb65
Ben Stone <benstone@uiowa.edu>
2022-05-25 09:01:35 -0500

    Add basic integer query with query display method

--- LearningLinq/Program.cs
+++ LearningLinq/Program.cs
@@ -1,9 +1,37 @@
-﻿namespace LearningLinq;
+﻿using System.Linq;
+using System.Collections;
+
+namespace LearningLinq;
 
 internal class Program
 {
     private static void Main()
     {
+        BasicIntQueries();
+    }
 
+    private static void BasicIntQueries()
+    {
+        var scores = new int[100];
+        for (var i = 0; i < scores.Length; i++)
+        {
+            scores[i] = i;
+        }
+
+        var greaterThanEightyQuery = scores.Where(x => x > 80);
+
+        DisplayQuery(greaterThanEightyQuery, "Greater than 80");
+
+
+    }
+
+    private static void DisplayQuery(IEnumerable query, string message)
+    {
+        Console.Write($"{message}: ");
+        foreach (var x in query)
+        {
+            Console.Write($"{x} ");
+        }
+        Console.WriteLine("\n");
     }
 }
